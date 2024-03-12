4535b499c4bac0e0b22d7f3285ded5daa69e9377
lattesprezzo <lattesprezzo@gmail.com>
2024-03-12 12:23:28 +0200

    Enemy LineRenderer activation building

--- Assets/Scripts/EnvironmentScanner.cs
+++ Assets/Scripts/EnvironmentScanner.cs
@@ -30,6 +30,8 @@ public class EnvironmentScanner : MonoBehaviour
         float RayMaxDistanceStart = position1.position.z;   
         rayMaxDistance = RayMaxDistanceEnd - RayMaxDistanceStart;
 
+        HitLayerName = HitLayerList[0]; 
+
         LayerMask layermask = LayerMask.GetMask(HitLayerName);
 
        // Ray rayForward = new(transform.position, position2.position);
@@ -42,21 +44,26 @@ public class EnvironmentScanner : MonoBehaviour
             Debug.Log("Hit object's layer: " + hitLayerName);
             // gameObject.SetActive(false);    
 
+           // LineRendererController();
         }
 
+    }
+    void LineRendererController()
+    {
+        lineRenderer.SetPosition(0, position1.position);
+      //  transform.rotation = Quaternion.Euler(rotation1, 0, 0);
+        lineRenderer.SetPosition(1, position2.position);   
     }
 
     void Start()
     {
-
+        lineRenderer.gameObject.SetActive(false);
     }
 
 
     void Update()
     {   
-        lineRenderer.SetPosition(0, position1.position);
-      //  transform.rotation = Quaternion.Euler(rotation1, 0, 0);
-        lineRenderer.SetPosition(1, position2.position);   
+
         
         RayCastController();    
 
